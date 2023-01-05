1a082fea875b1da54de575753d0f1210595198e2
ycshang <351234359@qq.com>
2023-01-05 22:33:26 +0800

    feat: eslint + Prettier + Husky 规范化代码

--- vite.config.ts
+++ vite.config.ts
@@ -2,57 +2,56 @@
  * @Author: ycshang
  * @Date: 2023-01-03 22:34:42
  * @LastEditors: ycshang
- * @LastEditTime: 2023-01-04 23:21:21
+ * @LastEditTime: 2023-01-05 22:14:21
  */
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import Unocss from "./config/unocss";
 
 const rollupOptions = {
-
-    external: ["vue", "vue-router"],
-    output: {
-        globals: {
-            vue: "Vue",
-        },
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
     },
+  },
 };
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        // 添加JSX插件
-        vueJsx({
-            // options are passed on to @vue/babel-plugin-jsx
-        }),
-        // 添加UnoCSS插件
-        Unocss()
-    ],
-    // 添加库模式配置
+  plugins: [
+    vue(),
+    // 添加JSX插件
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
+    // 添加UnoCSS插件
+    Unocss(),
+  ],
+  // 添加库模式配置
 
-    build: {
-        rollupOptions,
-        minify: false,
-        cssCodeSplit: true,
-        lib: {
-            entry: "./src/entry.ts",
-            name: "SSYUI",
-            fileName: "ssy-ui",
-            formats: ["esm", "umd", "iife"],
-        },
+  build: {
+    rollupOptions,
+    minify: false,
+    cssCodeSplit: true,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SSYUI",
+      fileName: "ssy-ui",
+      formats: ["esm", "umd", "iife"],
     },
+  },
 
-    test: {
-        // enable jest-like global test APIs
-        globals: true,
-        // simulate DOM with happy-dom
-        // (requires installing happy-dom as a peer dependency)
-        environment: 'happy-dom',
-        // 支持tsx组件，很关键
-        transformMode: {
-            web: [/.[tj]sx$/]
-        }
-    }
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: "happy-dom",
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/],
+    },
+  },
 });
