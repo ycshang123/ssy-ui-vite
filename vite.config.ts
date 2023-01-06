d954da4279f490c9ff5fd00fa87edc58017762fc
ycshang <351234359@qq.com>
2023-01-06 16:33:28 +0800

    fix: 修改测试

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@
  * @Author: ycshang
  * @Date: 2023-01-03 22:34:42
  * @LastEditors: ycshang
- * @LastEditTime: 2023-01-05 22:45:55
+ * @LastEditTime: 2023-01-06 16:22:51
  */
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import Unocss from "./config/unocss";
+import UnoCss from "./config/unocss";
+import { UserConfig } from "vitest";
 
 const rollupOptions = {
-  external: ["vue", "vue-router"],
+  external: ["vue"],
   output: {
     globals: {
       vue: "Vue",
@@ -19,32 +20,22 @@ const rollupOptions = {
   },
 };
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    // 添加JSX插件
-    vueJsx({
-      // options are passed on to @vue/babel-plugin-jsx
-    }),
-    // 添加UnoCSS插件
-    Unocss(),
-  ],
-  // 添加库模式配置
-
+export const config = {
+  plugins: [vue(), vueJsx(), UnoCss()],
   build: {
     rollupOptions,
-    minify: "terser", // boolean | 'terser' | 'esbuild'
+    minify: `terser`, // boolean | 'terser' | 'esbuild'
     sourcemap: true, // 输出单独 source文件
     brotliSize: true, // 生成压缩大小报告
-    cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
       name: "SSYUI",
       fileName: "ssy-ui",
       formats: ["esm", "umd", "iife"], // 导出模块类型
     },
+    outDir: "./dist",
+    cssCodeSplit: true,
   },
-
   test: {
     // enable jest-like global test APIs
     globals: true,
@@ -55,5 +46,12 @@ export default defineConfig({
     transformMode: {
       web: [/.[tj]sx$/],
     },
+    coverage: {
+      provider: "istanbul", // or 'c8',
+      reporter: ["text", "json", "html"],
+    },
   },
-});
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(config as UserConfig);
